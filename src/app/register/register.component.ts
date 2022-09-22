53641d8d5e4db0b4c4d68a8dccef772f0fe95eec
ATOM_MAX <52504909+Abilash2001@users.noreply.github.com>
2022-09-23 00:31:24 +0530

    started api

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import axios from 'axios';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  error:string="";
+  name:string="";
+  email:string="";
+  phone:string="";
+  password:string="";
 
-  constructor() { }
+  constructor(router: ActivatedRoute) { 
+    router.queryParams.subscribe((params) => {return this.error = params['error']})
+  }
 
   ngOnInit(): void {
   }
-
+  addNewUser = async () => {
+    try
+    {
+        await axios.post("http://localhost:8000/sign",{params: {
+        name: this.name,
+        email:this.email,
+        password:this.password
+      }})
+    }catch(e)
+    {
+      console.log(e)
+    }
+  }
 }
