21a2bce14542ff00f6e5a25f78223b03b1e51d34
ATOM_MAX <52504909+Abilash2001@users.noreply.github.com>
2022-09-23 16:15:24 +0530

    fixed post data network call

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import axios from 'axios';
+import * as FormData from 'form-data';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -12,21 +13,29 @@ export class RegisterComponent implements OnInit {
   email:string="";
   phone:string="";
   password:string="";
-
+  csrf:any;
   constructor(router: ActivatedRoute) { 
+
     router.queryParams.subscribe((params) => {return this.error = params['error']})
   }
 
   ngOnInit(): void {
   }
+
   addNewUser = async () => {
+    const data = {name: this.name, email: this.email,phone: this.phone ,password: this.password}
     try
     {
-        await axios.post("http://localhost:8000/sign",{params: {
-        name: this.name,
-        email:this.email,
-        password:this.password
-      }})
+        let newForm = new FormData()
+        newForm.append('name',this.name)
+        newForm.append('email',this.email)
+        newForm.append('phone',this.phone)
+        newForm.append('password',this.password)
+        
+        const resp = await axios.post("http://localhost:8000/sign",newForm)
+        window.location=resp.data;
+
+        
     }catch(e)
     {
       console.log(e)
